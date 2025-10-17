ffb9fd7a085dd8335e50b4ba4287efb871709699
Andri Saputra <andri.saputra25021@gmail.com>
2025-10-17 13:06:47 +0700

    update file html, css, dan js untuk revisi DOM pada navbar

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ const navlist = document.querySelector(".navlist");
 menu.addEventListener("click", () => {
   navlist.classList.toggle("active");
 });
+
+document.addEventListener("click", (event) => {
+  if (!menu.contains(event.target) && !navlist.contains(event.target)) {
+    navlist.classList.remove("active");
+  }
+});
